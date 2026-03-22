394882c5949003ffe1f0b3c22ebf99cc4b91e3c9
Sanjay <your-email@example.com>
2026-03-22 13:38:11 +0530

    Added GET API to fetch items from DB

--- SQLite3/SQL_connection.py
+++ SQLite3/SQL_connection.py
@@ -10,16 +10,17 @@ cursor=conn.cursor()
 
 #table creation made here
 
-# cursor.execute('''
-#        create table if not exists items(
-#             item_id integer primary key autoincrement,
-#             name text not null,
-#             des text
-#     )
-#     ''')
+cursor.execute('''
+       create table if not exists items(
+            item_id integer primary key ,
+            name text not null,
+            des text
+    )
+    ''')
 
-# conn.commit()
+conn.commit()
 class item(BaseModel):
+    ID:int
     name:str
     des:str
 
@@ -27,9 +28,41 @@ class item(BaseModel):
 @app.post("/items/create")
 def create_items(i:item):
     try:
-        cursor.execute("INSERT INTO items (name, des) VALUES (?,?)",(i.name, i.des))
+
+        # check duplicate
+        cursor.execute("select * from items where item_id=? ",(i.ID,))
+        existing=cursor.fetchone()
+
+        if existing:
+            return{
+                "message": "Item already exists",
+                "item_id": existing[0]
+            }
+        cursor.execute("INSERT INTO items (item_id,name, des) VALUES (?,?,?)",(i.ID,i.name,i.des))
         conn.commit()
-        return {"message": "Item created successfully"}
+        return {"message": "Item created successfully",
+                "name":i.name,
+                "des":i.des}
+        
     except Exception as e:
-        raise HTTPException(status_code=400,detail=str(e),detail="Internal Server Error")
+        raise HTTPException(status_code=500,detail=str(e))
+    
+
+@app.get("/items/read")
+def read_items():
+    try:
+        cursor.execute("select * from items")
+        rows=cursor.fetchall()
+        result=[]
+        for row in rows:
+            result.append({
+                "id":row[0],
+                "name":row[1],
+                "des":row[2]
+            })
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
+
+         
      
